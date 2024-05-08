3a1c73b2f4a1246346dea1bbd4fe6d6a4447c142
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-05-08 10:40:12 +0700

    remove int value from OfpMsg

--- src/openflow/ofp_manager/ofp_message.rs
+++ src/openflow/ofp_manager/ofp_message.rs
@@ -1,7 +1,7 @@
 pub enum OfpMsg {
-    Hello = 0,
-    FeaturesReq = 5,
-    PacketIn = 8,
-    FlowMod = 14,
-    NotFound = -1,
+    Hello,
+    FeaturesReq,
+    PacketIn,
+    FlowMod,
+    NotFound
 }
