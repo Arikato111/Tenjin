a4ddcb00917cf10a4c3313a1a8707a5ae2caf3ea
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-07-24 21:18:08 +0700

    add derve clone for controller10

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use tenjin::example;
-use tenjin::openflow::ofp13::ControllerFrame13;
+use tenjin::{example, openflow::ofp10::ControllerFrame10};
 extern crate byteorder;
 
 fn main() -> Result<(), std::io::Error> {
-    let controller = example::Controller13::new();
+    let controller = example::Controller10::new();
     controller.listener("127.0.0.1:6633");
     Ok(())
 }
